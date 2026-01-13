d4765007372cbbaa8927f72922d42ffc564489ce
rejoller <130197750+rejoller@users.noreply.github.com>
2026-01-13 11:07:42 +0300

    сырая версия с pyrogram для получения меню из федерации

--- dags/menu_review_dag.py
+++ dags/menu_review_dag.py
@@ -6,8 +6,11 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from openai import OpenAI
 import pytesseract
 from PIL import Image
+from pdf2image import convert_from_path
 import requests
 import telebot
+from pyrogram import Client
+import tempfile
 
 
 from datetime import datetime as dt, timedelta
@@ -103,8 +106,133 @@ with DAG(
         return review_text
     
     
+    
+    
+    
     @task
-    def send_menu_review_to_telegram(review_text):
+    def get_fed_menu():
+        
+        api_id = Variable.get("api_id")
+        api_hash = Variable.get("api_hash")
+        cafetera_chat_id = Variable.get("cafetera_chat_id")
+        
+        app = Client("my_account", api_id, api_hash)
+        app.start()
+        
+        
+        date = dt.date
+        for message in app.get_chat_history(chat_id=cafetera_chat_id, limit=3):
+
+            if message.document:
+                for document in message.document.file_name:
+                    if '7' in document:
+                        date_7_file = date(message.document.date)
+                        floor_7_filepath = f'menu_path/7_floor_{date_7_file}.pdf'
+                        
+                        message.download(floor_7_filepath)
+
+                    
+                    if '1' in document:
+                        date_1_file = date(message.document.date)
+                        floor_1_filepath = f'menu_path/1_floor_{date_1_file}.pdf'
+                        
+                        message.download(floor_1_filepath)
+        
+        menu_dict = {'7_floor': floor_7_filepath, '1_floor': floor_1_filepath}
+        
+        return menu_dict
+    
+    @task
+    def federation_menu_recognition(menu_dict):
+        
+        floor_7_filepath = menu_dict['7_floor']
+        floor_1_filepath = menu_dict['1_floor']
+        
+        if floor_7_filepath:
+            floor_1_text_result = []
+
+            with tempfile.TemporaryDirectory() as tmpdir:
+                pages = convert_from_path(
+                    floor_7_filepath,
+                    dpi=300,              
+                    fmt="png",
+                    output_folder=tmpdir
+                )
+
+                for i, page in enumerate(pages):
+                    img_path = os.path.join(tmpdir, f"page_{i}.png")
+                    page.save(img_path, "PNG")
+
+                    text = pytesseract.image_to_string(
+                        Image.open(img_path),
+                        lang="rus+eng",
+                        config="--oem 3 --psm 6"
+                    )
+                    floor_1_text_result.append(text)
+
+        full_7_floor_text = "\n\n".join(floor_1_text_result)
+        
+        
+        if floor_1_filepath:
+            floor_1_text_result = []
+            
+            with tempfile.TemporaryDirectory() as tmpdir:
+                pages = convert_from_path(
+                    floor_1_filepath,
+                    dpi=300,              
+                    fmt="png",
+                    output_folder=tmpdir
+                )
+
+                for i, page in enumerate(pages):
+                    img_path = os.path.join(tmpdir, f"page_{i}.png")
+                    page.save(img_path, "PNG")
+
+                    text = pytesseract.image_to_string(
+                        Image.open(img_path),
+                        lang="rus+eng",
+                        config="--oem 3 --psm 6"
+                    )
+                    floor_1_text_result.append(text)
+
+        full_1_floor_text = "\n\n".join(floor_1_text_result)
+
+        federation_menu_dict = {'7_floor': full_7_floor_text, '1_floor': full_1_floor_text}
+        
+        return federation_menu_dict
+            
+    @task
+    def get_federation_openai_text(federation_menu_dict):
+        openai_api_key = Variable.get("openai_api_key")
+        client = OpenAI(api_key=openai_api_key)
+        
+        
+        gpt_response_1_floor= client.responses.create(
+            model = 'gpt-5',
+            input=f""" сделай краткое меню бизнес ланча за 305 рублей на основе текста:
+            {federation_menu_dict['1_floor']} Используй только такие HTML теги: <b>, <i>. Не используй никакие другие теги"""),
+        
+        review_1_floor = gpt_response_1_floor.output_text
+        
+        gpt_response_7_floor= client.responses.create(
+            model = 'gpt-5',
+            input = f""" сделай краткое меню бизнес ланча за 305 рублей на основе текста:
+            {federation_menu_dict['7_floor']} Используй только такие HTML теги: <b>, <i>. Не используй никакие другие теги """)
+        
+        review_7_floor = gpt_response_7_floor.output_text
+        
+        review_1_and_7_floor = {'1_floor': review_1_floor, '7_floor': review_7_floor}
+
+        return review_1_and_7_floor
+    
+        
+
+    
+    @task
+    def send_menu_review_to_telegram(review_text, review_1_and_7_floor):
+        
+        floor_1_text = review_1_and_7_floor['1_floor']
+        floor_7_text = review_1_and_7_floor['7_floor']
         
         bot = telebot.TeleBot(token=Variable.get("BOT_TOKEN"))
         
@@ -112,7 +240,7 @@ with DAG(
         conn = pg_hook.get_conn()
         session = conn.cursor()
         
-        
+        text = f"""{review_text}\n\n меню первого этажа: <blockquote expandable>{floor_1_text}</blockquote>\n\n меню второго этажа: <blockquote expandable>{floor_7_text}</blockquote>\n\n"""
         ids_query = """select distinct user_id from subscriber"""
         session.execute(ids_query)
         ids = session.fetchall()
@@ -120,7 +248,7 @@ with DAG(
         
         for id in chat_ids:
             try:
-                bot.send_message(chat_id=id, text=review_text, parse_mode='HTML')
+                bot.send_message(chat_id=id, text=text, parse_mode='HTML')
             except Exception as e:
                 logging.error(f"Failed to send message to {id} {e}")
                 
@@ -132,8 +260,12 @@ with DAG(
 
     filepath = import_menu_from_website()
     response_tesseract = get_tesseract_recognition(filepath)
+    menu_dict = get_fed_menu()
+    federation_menu_dict = federation_menu_recognition(menu_dict)
+    
+    review_1_and_7_floor = get_federation_openai_text(federation_menu_dict)
     review_text = get_openai_text(response_tesseract)
-    send_menu_review_to_telegram(review_text)
+    send_menu_review_to_telegram(review_text, review_1_and_7_floor)
     
     
     
